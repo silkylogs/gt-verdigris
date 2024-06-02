ec8824a06383b96b159d5c5315c258479ff7daa2
silkylogs <silkylogs@gmail.com>
2024-06-02 23:56:39 +0600

    Rename platform_raylib.c to platform_raylib_win32.c

--- c/build_cradle.bat
+++ c/build_cradle.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cl.exe src\platform_raylib.c /Zi /nologo ^
+cl.exe src\platform_raylib_win32.c /Zi /nologo ^
 /I"third_party\raylib-5.0_win64_msvc16\include" "third_party\raylib-5.0_win64_msvc16\lib\raylib.lib" ^
 opengl32.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib
